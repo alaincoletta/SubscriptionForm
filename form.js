b2b1baa04e97ce1f12d24e2730750adfbe5b24d7
alaincoletta <alaincoletta@gmail.com>
2021-02-28 10:56:25 +0100

    Initial commit

---
+++ form.js
@@ -0,0 +1,55 @@
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <p>
+        <label>
+          Naam:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        </p>
+        <p>
+        <label>
+          Voornaam:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        </p>
+        <p>
+        <label>
+          gebordaatum:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        </p>
+        <p>
+        <label>
+          email:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        </p>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('root')
+);
